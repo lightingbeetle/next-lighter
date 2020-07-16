8be8c4b831b19cd6cc25433401a9f647d9ab3b95
Adam Mockor <adam.mockor@gmail.com>
2020-07-16 11:40:30 +0200

    add support fot MDX pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <ul>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
 }
