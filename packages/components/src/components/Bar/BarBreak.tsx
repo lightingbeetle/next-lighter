10edeae5c44422b5883d766ac678e3c8008b6c87
Dusan Tuzinsky <brooklynPD@gmail.com>
2021-04-01 16:15:02 +0200

    set types for bar break

--- packages/components/src/components/Bar/BarBreak.tsx
+++ packages/components/src/components/Bar/BarBreak.tsx
@@ -3,7 +3,9 @@ import cx from "classnames";
 
 const CLASS_ROOT = "bar__break";
 
-const BarBreak = ({ className, ...other }) => {
+type BarBreakProps = JSX.IntrinsicElements["div"];
+
+const BarBreak = ({ className, ...other }: BarBreakProps) => {
   const classes = cx(CLASS_ROOT, className);
 
   return <div className={classes} {...other} />;
