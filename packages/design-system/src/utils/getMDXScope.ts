1374d167eb669fe4167960dc5b84f896a9e3d5c3
Adam Mockor <adam.mockor@gmail.com>
2021-04-04 13:11:51 +0200

    Props will handle component without __docgenInfo

--- packages/design-system/src/utils/getMDXScope.ts
+++ packages/design-system/src/utils/getMDXScope.ts
@@ -3,13 +3,13 @@ import { Button, Icon, Select } from "components";
 export default function getMDXScope() {
   return {
     // TODO: We should not name all possible components here
-    button: { __docgenInfo: Button.__docgenInfo },
-    select: { __docgenInfo: Select.__docgenInfo },
-    icon: { __docgenInfo: Icon.__docgenInfo },
+    button: { __docgenInfo: Button.__docgenInfo ?? null },
+    select: { __docgenInfo: Select.__docgenInfo ?? null },
+    icon: { __docgenInfo: Icon.__docgenInfo ?? null },
     selectItems: [
       { label: "Item 1", value: "item 1" },
       { label: "Item 2", value: "item 2" },
-      { label: "Item 3", value: "item 3" }
-    ]
+      { label: "Item 3", value: "item 3" },
+    ],
   };
 }
