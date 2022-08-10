e6f8cbb75dcaf2691c627a8d94ec349d60a1a1c4
Benjamín Starý <stary.benjamin@gmail.com>
2022-08-10 13:32:15 +0200

    setup jest-axe

--- packages/components/jest-setup.ts
+++ packages/components/jest-setup.ts
@@ -1,5 +1,9 @@
 import "@testing-library/jest-dom";
 import React from "react";
+import { toHaveNoViolations } from "jest-axe";
 
 // to hide Warning: useLayoutEffect does nothing on the server
 React.useLayoutEffect = React.useEffect;
+
+// extend expect with jest-axe assertions
+expect.extend(toHaveNoViolations);
