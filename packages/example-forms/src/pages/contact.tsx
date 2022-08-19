fe7c30190680d133938cd1f26766215c1d7f34dc
Adam Močkoř <adam.mockor@gmail.com>
2022-08-19 14:09:11 +0200

    streamline contact form implementation

--- packages/example-forms/src/pages/contact.tsx
+++ packages/example-forms/src/pages/contact.tsx
@@ -1,16 +1,9 @@
-import React, { /* useRef, */ useState } from "react";
+import React from "react";
 import { FormProvider, useForm } from "react-hook-form";
-// import ReCAPTCHA from "react-google-recaptcha";
 import { Input, TextArea, Error } from "components";
-import ApprovalCheckbox from "../components/ApprovalCheckbox";
+import MarketingCheckbox from "../components/MarketingCheckbox";
 
 function ContactForm() {
-  // ReCaptcha is not easy to work with on localhost
-  // TODO: disable recaptcha in DEV environment
-  // const recaptchaRef = useRef<ReCAPTCHA>(null);
-  const [isSuccess, setSuccess] = useState(false);
-  const [formError, setFormError] = useState(null);
-
   const methods = useForm({
     defaultValues: {
       name: "",
@@ -18,18 +11,25 @@ function ContactForm() {
       phoneNumber: "",
       message: "",
       marketing: false,
+      // This is just to make the "field" available in TypeScript. It will never
+      // have a value assigned to it and JSON.stringify will filter it out.
+      serverError: undefined,
     },
   });
 
-  const { handleSubmit } = methods;
+  const {
+    handleSubmit,
+    reset,
+    setError,
+    clearErrors,
+    formState: { isSubmitSuccessful, errors },
+  } = methods;
 
+  // onSubmit will not be called if there are validation errors in formState.errors
   const onSubmit = async (data) => {
-    // const token = await recaptchaRef.current.executeAsync();
-    // recaptchaRef.current.reset();
-
     const response = await fetch("/api/contact", {
       method: "POST",
-      body: JSON.stringify({ ...data /* token */ }),
+      body: JSON.stringify({ ...data }),
       headers: {
         "Content-Type": "application/json",
       },
@@ -38,11 +38,17 @@ function ContactForm() {
     const { status, error } = await response.json();
 
     if (!status || error) {
-      setFormError(error);
+      // this could be more granularised if we do server side validation of fields for example
+      setError("serverError", {
+        type: "server",
+        message: error,
+      });
+
       return;
     }
 
-    setSuccess(true);
+    // reset entire form to default values after submit success
+    reset();
   };
 
   // TODO: don't create component in the render - memoize or refactor out
@@ -50,78 +56,69 @@ function ContactForm() {
     <div>
       <h3>Ďakujeme!</h3>
       <p>Vaša správa bola úspešne odoslaná. Čoskoro vás budeme kontaktovať.</p>
-      <button type="button" onClick={() => setSuccess(false)}>
-        Späť
-      </button>
     </div>
   );
 
-  if (isSuccess) {
-    return <SuccessMessage />;
-  }
-
   return (
-    <FormProvider {...methods}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Input
-          type="text"
-          id="name"
-          label="Meno"
-          isRequired
-          messages={{
-            required: "Meno je povinný údaj",
-          }}
-          {...methods.register("name", { required: true })}
-        />
-        <Input
-          type="email"
-          pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+$"
-          id="email"
-          label="Email"
-          isRequired
-          messages={{
-            required: "Email je povinný údaj",
-          }}
-          {...methods.register("email", { required: true })}
-        />
-        <Input
-          type="tel"
-          id="phoneNumber"
-          label="Telefónne číslo"
-          hint="Musí začínať 00 alebo +"
-          {...methods.register("phoneNumber", {
-            pattern: /^(\+|00)[0-9]{7,32}$/,
-          })}
-          messages={{
-            pattern: "Číslo musí byť v medzinárodnom formáte",
-          }}
-        />
-        <TextArea
-          label="Vaša správa"
-          id="message"
-          {...methods.register("message")}
-        />
-        <p>
-          Spoločnosť XXX je oprávnená spracúvať moje osobné údaje vyplnené v
-          kontaktnom formulári na účely spracovania mojej požiadavky. Oboznámil
-          (a) som sa s informáciami v sekcii{" "}
-          <a href="/" target="_blank" rel="noreferrer">
-            Ochrana osobných údajov
-          </a>
-        </p>
-        <ApprovalCheckbox {...methods.register("marketing")} />
-        {formError && <Error>{formError}</Error>}
-        {/* <ReCAPTCHA
-          ref={recaptchaRef}
-          size="invisible"
-          badge="inline"
-          sitekey={
-            process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? "NOT_AVAILABLE"
-          }
-        /> */}
-        <button type="submit">Odoslať</button>
-      </form>
-    </FormProvider>
+    <>
+      {isSubmitSuccessful && <SuccessMessage />}
+      <FormProvider {...methods}>
+        {/* noValidate will disable browser native validation */}
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <Input
+            type="text"
+            id="name"
+            label="Meno"
+            isRequired
+            messages={{
+              required: "Meno je povinný údaj",
+            }}
+            {...methods.register("name", { required: true })}
+          />
+          <Input
+            type="email"
+            pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+$"
+            id="email"
+            label="Email"
+            isRequired
+            messages={{
+              required: "Email je povinný údaj",
+            }}
+            {...methods.register("email", { required: true })}
+          />
+          <Input
+            type="tel"
+            id="phoneNumber"
+            label="Telefónne číslo"
+            hint="Musí začínať 00 alebo +"
+            {...methods.register("phoneNumber", {
+              pattern: /^(\+|00)[0-9]{7,32}$/,
+            })}
+            messages={{
+              pattern: "Číslo musí byť v medzinárodnom formáte",
+            }}
+          />
+          <TextArea
+            label="Vaša správa"
+            id="message"
+            {...methods.register("message")}
+          />
+          <p>
+            Spoločnosť XXX je oprávnená spracúvať moje osobné údaje vyplnené v
+            kontaktnom formulári na účely spracovania mojej požiadavky.
+            Oboznámil (a) som sa s informáciami v sekcii{" "}
+            <a href="/" target="_blank" rel="noreferrer">
+              Ochrana osobných údajov
+            </a>
+          </p>
+          <MarketingCheckbox {...methods.register("marketing")} />
+          {errors?.serverError && <Error>{errors.serverError.message}</Error>}
+          <button onClick={() => clearErrors("serverError")} type="submit">
+            Odoslať
+          </button>
+        </form>
+      </FormProvider>
+    </>
   );
 }
 
