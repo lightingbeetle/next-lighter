8d289b57f7c11c05c78e812cd24f83ea616b0898
Adam Močkoř <adam.mockor@gmail.com>
2022-08-23 12:49:50 +0200

    simplify example-form next config

--- packages/example-forms/src/pages/_app.tsx
+++ packages/example-forms/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "modern-normalize/modern-normalize.css";
+import "components/styles.scss";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
