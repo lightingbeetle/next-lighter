fed9afcfaa8af252307c1d078dd917daef681671
Adam Močkoř <adam.mockor@gmail.com>
2022-10-11 16:12:05 +0200

    fix local db path

--- packages/example-strapi/backend/config/database.js
+++ packages/example-strapi/backend/config/database.js
@@ -1,12 +1,14 @@
 const path = require("path");
 
+console.log(path.join(__dirname, "..", ".tmp/data.db"));
+
 module.exports = ({ env }) => ({
   connection: {
     client: "sqlite",
     connection: {
       filename: env(
         "DATABASE_FILENAME",
-        path.join(__dirname, "..", ".tmp/data.db")
+        path.join(__dirname, "..", "..", ".tmp/data.db")
       ),
     },
     useNullAsDefault: true,
