73acde2bf7f2862a7f105e5804508f95b1af8958
Matej Beno <matobeno1@gmail.com>
2023-01-25 11:08:50 +0100

    Adding Button to storybook

--- packages/example-storybook-design-system/stories/Button.stories.tsx
+++ packages/example-storybook-design-system/stories/Button.stories.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {ComponentProps} from 'react';
+import {Meta, Story} from "@storybook/react";
 
-import { Button } from './Button';
+import { Button } from "components";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -10,31 +11,31 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
-};
+} as Meta;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <Button {...args} />;
+const Template: Story<ComponentProps<typeof Button>> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  variant: 'plain',
+  children: 'Button'
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  children: 'Button'
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+  size: 'l',
+  children: 'Button'
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+  size: 's',
+  children: 'Button'
 };
