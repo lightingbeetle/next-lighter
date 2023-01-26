df6d1458ca44ed61b0cae2d918769d85b01028ee
Matej Beno <matobeno1@gmail.com>
2023-01-26 12:56:18 +0100

    Storybook adding a11y addons and updating testing

--- packages/example-storybook-design-system/stories/Button.stories.tsx
+++ packages/example-storybook-design-system/stories/Button.stories.tsx
@@ -1,60 +1,89 @@
-import React, {ComponentProps} from 'react';
+import React, {ComponentProps} from "react";
 import {Meta, Story} from "@storybook/react";
+import {userEvent, within, createEvent, fireEvent} from "@storybook/testing-library";
+import {expect} from "@storybook/jest";
 
-import { Button } from "components";
+import {Button} from "components";
 
-type ButtonProps = ComponentProps<typeof Button>
+type ButtonProps = ComponentProps<typeof Button>;
 
-type X = ButtonProps['purpose']
+type X = ButtonProps["purpose"];
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     square: {
-      control: 'boolean',
-      defaultValue: false
+      control: "boolean",
+      defaultValue: false,
     },
     isActive: {
-      control: 'boolean',
-      defaultValue: false
+      control: "boolean",
+      defaultValue: false,
     },
     isDisabled: {
-      control: 'boolean',
-      defaultValue: false
+      control: "boolean",
+      defaultValue: false,
     },
     purpose: {
-      control: 'select',
-      options: ['link', 'secondary', undefined]
-    }
+      control: "select",
+      options: ["link", "secondary", undefined],
+    },
   },
 } as Meta<ButtonProps>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: Story<ComponentProps<typeof Button>> = (args) => <Button {...args} />;
+const Template: Story<ComponentProps<typeof Button>> = (args) => (
+  <Button {...args} />
+);
+
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  variant: 'plain',
-  children: 'Button'
+  variant: "plain",
+  children: "Button",
 };
+
+export const Link = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Link.args = {
+  variant: "plain",
+  children: "Link to Storybook homepage",
+  href: "https://storybook.js.org",
+  purpose: 'link'
+};
+Link.play = async ({canvasElement}) => {
+  // Assigns canvas to the component root element
+  const canvas = within(canvasElement);
+  const element = canvas.getByRole('link')
+  const event = createEvent.click(element)
+  event.preventDefault()
+  fireEvent(element, event)
+
+  expect(element).toHaveAttribute(
+    "href",
+    "https://storybook.js.org"
+  );
+  expect(element.tagName.toLowerCase()).toBe('a')
+};
+
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  children: 'Button'
+  children: "Button",
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'l',
-  children: 'Button'
+  size: "l",
+  children: "Button",
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 's',
-  children: 'Button'
+  size: "s",
+  children: "Button",
 };
