29673d2c4679b3e12bb66fc6cd1d396645332499
Adam Močkoř <adam.mockor@gmail.com>
2023-03-14 21:56:30 +0100

    replace Card component with Card from components package

--- packages/example-t3/src/components/Card/index.tsx
+++
@@ -1,8 +0,0 @@
-import Card from "./Card";
-import CardSection from "./CardSection";
-import CardSectionImage from "./CardSectionImage";
-import CardTitle from "./CardTitle";
-import CardAction from "./CardAction";
-
-export { CardSection, CardSectionImage, CardTitle, CardAction };
-export default Card;
